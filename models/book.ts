591cab63cfa8202eeea2f293af9b0d8a783bd3e0
Kyra Buchmann <kac580@outlook.com>
2020-03-21 21:22:42 -0500

    first commit

---
+++ models/book.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from 'mongoose';
+
+const BookSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: { type: String, required: true },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model('books', BookSchema);
---
+++ routes/book-route.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import Books from '../models/book';  
+export class BookRoute {
+
+    public bookRoute(app): void {
+      app.route('/api/').get((req: Request, res: Response, next: NextFunction) => {
+        Books.find((err, books) => {
+          if (err) { return next(err); }
+          res.json(books);
+        });
+    });
+
+    app.route('/api/:id').get((req: Request, res: Response, next: NextFunction) => {
+      Books.findById(req.params.id, (err, book) => {
+        if (err) { return next(err); }
+        res.json(book);
+      });
+    });
+
+    app.route('/api/').post((req: Request, res: Response, next: NextFunction) => {
+      console.log(req.body);
+      Books.create(req.body, (err, book) => {
+        if (err) { return next(err); }
+        res.json(book);
+      });
+    });
+    app.route('/api/:id').put((req: Request, res: Response, next: NextFunction) => {
+        Books.findByIdAndUpdate(req.params.id, req.body, (err, book) => {
+          if (err) { return next(err); }
+          res.json(book);
+        });
+      });
+  
+      app.route('/api/:id').delete((req: Request, res: Response, next: NextFunction) => {
+        Books.findByIdAndRemove(req.params.id, req.body, (err, book) => {
+          if (err) { return next(err); }
+          res.json(book);
+        });
+      });
+    }
+  }
